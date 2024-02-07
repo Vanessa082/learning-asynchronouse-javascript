02ad5031a9ca429665527bc21be5af1ddd2f464d
vanessa082 <wahvanessa22@gmail.com>
2024-02-08 00:57:19 +0100

    worked on the style.css, edited the various sections changed the div to nav in index.html added media query

--- script/index.js
+++ script/index.js
@@ -10,7 +10,18 @@
 // console.log("Unknown weather type!");
 // break; }
 
+// http request && status code
+
 const request = new XMLHttpRequest
+
+request.addEventListener('readystatechange',() => {
+    // console.log(request, request.readyState)
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request, request.responseText)
+    } else if(request.readyState){
+        console.log('coould not fetch the data')
+    }
+})
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send()
